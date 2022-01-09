1e4df508ecd1d7630603760f1b2a047374d72d2a
p_penkovskiy <p.penkovskiy@gmail.com>
2022-01-10 00:27:29 +0300

    commit18

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -120,10 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 MEDIA_ULR = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
