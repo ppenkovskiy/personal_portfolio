5cabfb8e863fa3fcf8715afe73a64c33b87df305
p_penkovskiy <p.penkovskiy@gmail.com>
2022-02-12 22:37:30 +0300

    commit_final

--- blog/urls.py
+++ blog/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
     path('<int:blog_id>/', views.detail, name='detail'),
 ]
+
--- portfolio/models.py
+++ portfolio/models.py
@@ -4,8 +4,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
-    url = models.URLField(blank=True)
-
+    url = models.URLField(blank=True)  # 'blank=True' means 'allow nulls'
 
     def __str__(self):
         return self.title
